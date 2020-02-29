21fdf2abb0e8fd549cc0c8aaf4fd0c476db9b453
MargoAndri <margoandriasyan@gmail.com>
2020-02-29 15:11:09 +0300

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require(`path`);
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.join(__dirname, "/dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+};
